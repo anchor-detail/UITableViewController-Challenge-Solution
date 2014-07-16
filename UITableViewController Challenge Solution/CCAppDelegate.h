5a6417dc42467acd373c664f002f870d5170b935
Ryan McCawley <ryan.mccawley@me.com>
2014-07-15 21:03:43 -0400

    Initial Commit

---
+++ UITableViewController Challenge Solution/CCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CCAppDelegate.h
+//  UITableViewController Challenge Solution
+//
+//  Created by McCawley on 7/15/14.
+//  Copyright (c) 2014 Anchor-Detail. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UITableViewController Challenge Solution/CCViewController.h
@@ -0,0 +1,13 @@
+//
+//  CCViewController.h
+//  UITableViewController Challenge Solution
+//
+//  Created by McCawley on 7/15/14.
+//  Copyright (c) 2014 Anchor-Detail. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCViewController : UIViewController
+
+@end
