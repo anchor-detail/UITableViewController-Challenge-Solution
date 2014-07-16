210728a3f47f1f9cf8fab634fc0ee8a8342f3905
Ryan McCawley <ryan.mccawley@me.com>
2014-07-15 21:41:17 -0400

    Added some color to CCTableViewController.m

--- UITableViewController Challenge Solution/CCTableViewController.h
+++ UITableViewController Challenge Solution/CCTableViewController.h
@@ -1,5 +1,5 @@
 //
-//  CCViewController.h
+//  CCTableViewController.h
 //  UITableViewController Challenge Solution
 //
 //  Created by McCawley on 7/15/14.
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface CCViewController : UIViewController
+@interface CCTableViewController : UITableViewController
 
 @end
